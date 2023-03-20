24dcd24db79e125d2de6f69a31db4b1fd988d9e0
th <65722663+1THGAMER1@users.noreply.github.com>
2023-03-20 17:18:52 +0100

    Margin Änderungen + Implementierung der MainView

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -8,9 +8,24 @@ namespace Project_P.MVVM.ViewModel
 {
     class MainViewModel : Core.ObservableObject
     {
+
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+            
         public MainViewModel()
         {
-
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
         }
 
     }
